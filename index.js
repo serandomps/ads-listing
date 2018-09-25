374db90cf8f479bde5c6b4dd900d177429cbecf3
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-09-25 19:51:43 +0530

    updating

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var utils = require('utils');
 
 dust.loadSource(dust.compile(require('./template'), 'advertisements-find'));
 
-module.exports = function (sandbox, options, done) {
+module.exports = function (ctx, sandbox, options, done) {
     dust.render('advertisements-find', options, function (err, out) {
         sandbox.append(out);
         sandbox.on('click', '.edit', function (e) {
-            serand.redirect($(this).closest('.thumbnail').attr('href') + '/edit');
+            serand.direct($(this).closest('.thumbnail').attr('href') + '/edit');
             return false;
         });
         done(null, function () {
